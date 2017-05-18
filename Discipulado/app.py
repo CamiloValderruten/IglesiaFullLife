4da07b4ca1bd71a0f8b031257bd79f5e0afe94e9
Camilo Valderruten <Josecamilo@me.com>
2017-05-18 19:05:59 -0400

    New Stuff

--- Discipulado/app.py
+++ Discipulado/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
-app = Flask(__name__)
 db = MongoClient(host="0.0.0.0")['fulllife']
 
 
-@app.route('/')
-def home():
-    category = request.args.get('category', 'preach')
-    media = db.media.find({"category": category})
-    return render_template('home.html', media=media)
+def create_app():
+    app = Flask(__name__)
 
+    @app.route('/')
+    def home():
+        category = request.args.get('category', 'preach')
+        media = db.media.find({"category": category})
+        return render_template('home.html', media=media)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    return app
