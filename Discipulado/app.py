232647a8405dbac0d9ccf3a92c41402383945213
Camilo Valderruten <Josecamilo@me.com>
2017-05-18 23:42:30 -0400

    Upload module

--- Discipulado/app.py
+++ Discipulado/app.py
@@ -1,16 +1,48 @@
 from flask import Flask, render_template, request
+from werkzeug import secure_filename
+import boto3
 from pymongo import MongoClient
 
 db = MongoClient(host="0.0.0.0")['fulllife']
+bucket = "full-life-media"
+password = "38knCo2G7BLfz0O"
+categories = ["discipulado", "preach", "snack", "youth"]
 
 
 def create_app():
     app = Flask(__name__)
+    s3 = boto3.client('s3')
 
-    @app.route('/')
+    @app.route('/', methods=['GET'])
     def home():
-        category = request.args.get('category', 'preach')
-        media = db.media.find({"category": category})
+        query = {"category": request.args.get('category', 'preach')}
+        if request.args.get('author'):
+            query['author'] = request.args.get('author')
+        media = db.media.find(query)
         return render_template('home.html', media=media)
+
+    @app.route('/upload', methods=['GET', 'POST'])
+    def upload():
+        if request.method == "POST" and request.form['password'] == password:
+            data = {"author": request.form['author'],
+                    "title": request.form['title'],
+                    "date": request.form['date'],
+                    "category": request.form['category'],
+                    "subcategory": request.form['subcategory']}
+
+            media = request.files.get('media')
+            key = secure_filename(media.filename)
+            data['type'] = 'audio' if key.split('.')[-1] == 'mp3' else 'video'
+            data['url'] = "https://s3.amazonaws.com/{}/{}".format(bucket, key)
+            s3.upload_fileobj(media, bucket, key)
+
+            poster = request.files.get('poster')
+            key = secure_filename(poster.filename)
+            data['poster_url'] = "https://s3.amazonaws.com/{}/{}".format(bucket,
+                                                                         key)
+            s3.upload_fileobj(poster, bucket, key)
+
+            db.media.save(data)
+        return render_template('upload.html', categories=categories)
 
     return app
