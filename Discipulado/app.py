94c90c3e1579cb878e34a88a457cc2ead41d34bb
Camilo Valderruten <Josecamilo@me.com>
2017-05-19 13:09:47 -0400

    Password

--- Discipulado/app.py
+++ Discipulado/app.py
@@ -1,43 +1,51 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from werkzeug import secure_filename
 import boto3
 from pymongo import MongoClient
 
 db = MongoClient(host="0.0.0.0")['fulllife']
 bucket = "full-life-media"
-password = "38knCo2G7BLfz0O"
-categories = ["discipulado", "preach", "snack", "youth"]
+a_password = "38knCo2G7BLfz0O"
+u_password = "fulllifestudent"
+secret = 'rR2y7S9wIl9#40f@F9oc8DTrW8OQZ67&t^Pr16gPH@KiEi!'
 
 
 def create_app():
     app = Flask(__name__)
     s3 = boto3.client('s3')
+    app.secret_key = secret
 
     @app.route('/', methods=['GET'])
     def home():
-        query = dict()
-        if request.args.get('category'):
-            query = {"category": request.args.get('category')}
-        if request.args.get('author'):
-            query['author'] = request.args.get('author')
-        media = db.media.find(query)
-        data = {"categories": db.media.distinct("category"), 'media': media}
-        return render_template('home.html', **data)
+        return render_template('home.html', db=db)
+
+    @app.route('/login', methods=['POST'])
+    def login():
+        if request.form['password'] == u_password:
+            session['logged_in'] = request.form['password']
+        return redirect(url_for('home'))
+
+    @app.route('/logout')
+    def logout():
+        session.pop('logged_in', None)
+        return redirect(url_for('home'))
 
     @app.route('/upload', methods=['GET', 'POST'])
     def upload():
-        if request.method == "POST" and request.form['password'] == password:
+        return_data = dict(db=db)
+        if request.method == "POST" and request.form['password'] == a_password:
             data = {"author": request.form['author'],
                     "title": request.form['title'],
                     "date": request.form['date'],
-                    "category": request.form['category'],
-                    "subcategory": request.form['subcategory']}
+                    "category": request.form['category'].lower(),
+                    "subcategory": request.form['subcategory'].lower()}
 
             media = request.files.get('media')
             key = secure_filename(media.filename)
             data['type'] = 'audio' if key.split('.')[-1] == 'mp3' else 'video'
             data['url'] = "https://s3.amazonaws.com/{}/{}".format(bucket, key)
             s3.upload_fileobj(media, bucket, key)
+            return_data['media_url'] = data['url']
 
             poster = request.files.get('poster')
             key = secure_filename(poster.filename)
@@ -46,6 +54,6 @@ def create_app():
             s3.upload_fileobj(poster, bucket, key)
 
             db.media.save(data)
-        return render_template('upload.html', categories=categories)
+        return render_template('upload.html', **return_data)
 
     return app
