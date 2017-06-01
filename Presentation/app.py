266bbf87ec2b29438b9a98424277157123ed8584
Camilo Valderruten <Josecamilo@me.com>
2017-06-01 16:28:57 -0400

    Maybebe

--- Presentation/app.py
+++ Presentation/app.py
@@ -1,23 +1,56 @@
 from flask import Flask
+from flask import request
 from flask import render_template
+from bson.json_util import dumps
+from flask_socketio import SocketIO
+from flask_socketio import emit
+from pymongo import MongoClient
 
-timers = [
-    {"title": "Starting", "minutes": 5, 'icon': 'adjust'},
-    {"title": "Worship", "minutes": 30, 'icon': 'music'},
-    {"title": "Interlude", "minutes": 5, 'icon': 'clock-o'},
-    {"title": "Preach", "minutes": 32, 'icon': 'commenting'},
-    {"title": "Ministering", "minutes": 8, 'icon': 'fire'},
-    {"title": "Offering", "minutes": 5, 'icon': 'money'},
-    {"title": "Conectate", "minutes": 3, 'icon': 'plug'},
-    {"title": "Despedida", "minutes": 1, 'icon': 'sign-out'}
-]
+db = MongoClient()['Presentation']
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'BNJqqX8cuQe^$U68DrxMb8aGSk8LaD0CKO$78WKB!ySM'
+socket = SocketIO(app)
 
 
-def create_app():
-    app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-    @app.route('/')
-    def home():
-        return render_template('home.html', timers=timers)
 
-    return app
+@app.route('/clock')
+def clock():
+    return render_template('clock.html')
+
+
+@app.route('/timers', methods=['POST', 'GET'])
+def timers():
+    if request.method == "GET":
+        return dumps(db.timers.find(request.args))
+    timer = request.get_json(force=True)
+    db.timers.save(timer)
+    return dumps(timer)
+
+
+@app.route('/categories', methods=['POST', 'GET'])
+def categories():
+    if request.method == "GET":
+        return dumps(db.categories.find())
+    category = request.get_json(force=True)
+    db.categories.save(category)
+    return dumps(category)
+
+
+@socket.on('start_timer')
+def start_timer(timer):
+    emit('clock_start', timer, broadcast=True)
+
+
+@socket.on('timer_done')
+def timer_done(timer):
+    emit('timer_done', timer, broadcast=True)
+
+
+@socket.on('timer_changed')
+def timer_changed(timer):
+    emit('timer_changed', timer, broadcast=True)
+
--- Presentation/run.py
+++ Presentation/run.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import socket, app
 
-app = create_app()
-app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    socket.run(app, debug=True)
